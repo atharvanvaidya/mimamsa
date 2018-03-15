de062b25dac5b817d3625465778165298f060977
Atharva Vaidya <atharvanvaidya@gmail.com>
2018-03-15 16:58:52 +0530

    Added Black Theme

--- round2/base.js
+++ round2/base.js
@@ -20,10 +20,12 @@ var sound = document.getElementById('alert');
 sound.load();
 console.log("Audio Track Loaded!");
 
+//Assigning Functions to their Respective Buttons
 resetTwo.addEventListener('click', resetTimerTwo);
 startSix.addEventListener('click', startTimerSix);
 resetSix.addEventListener('click', resetTimerSix);
 
+//Resets all the Timers to Default Values
 function resetAll(){
   cSecTwo.innerHTML = "00";
   cMinTwo.innerHTML = "00";
@@ -35,6 +37,7 @@ function resetAll(){
   tMinSix.innerHTML = "06";
 }
 
+//Adding 0 Before the Numbers less than 10
 function stylize(number) {
   if(number < 10){
     var finalString = "0"+number;
@@ -45,14 +48,17 @@ function stylize(number) {
   }
 }
 
+//Converts Total Seconds to MM:SS
 function toMMSS(totalSeconds) {
   return (stylize(parseInt(totalSeconds / 60)) + ":" + stylize(totalSeconds % 60));
 }
 
+//Converts MM:SS to Total Seconds
 function toTotalSeconds(minutes , seconds) {
   return parseInt(parseInt(minutes) * 60 + parseInt(seconds));
 }
 
+//Main Counter Function which is called after EVERY 1 Second
 function countup() {
   counterTwo += 1;
   counterSix += 1;
@@ -60,38 +66,63 @@ function countup() {
   cMinTwo.innerHTML = stylize(parseInt(counterTwo / 60));
   cSecSix.innerHTML = stylize(counterSix % 60);
   cMinSix.innerHTML = stylize(parseInt(counterSix / 60));
+
+  //After Completion of the 2 Minutes Timer
   if (counterTwo == toTotalSeconds(tMinTwo.innerHTML , tSecTwo.innerHTML)) {
-    resetTimerTwo();
+    //resetTimerTwo();
+    cSecTwo.style.color = "red";
+    cMinTwo.style.color = "red";
     sound.play();
   }
+
+  //After Completion of the 6 Minutes Timer
   if (counterSix == toTotalSeconds(tMinSix.innerHTML , tSecSix.innerHTML)) {
     clearInterval(tog);
     sound.play();
     alert("Total Time Elapsed!");
   }
+
+  //Blinking of the Last 20 Seconds
+  if(counterTwo > 100 && counterTwo < 120){
+    if (counterTwo % 2 == 0) {
+      cSecTwo.style.color = "red";
+      cMinTwo.style.color = "red";
+    }
+    else {
+      cSecTwo.style.color = "white";
+      cMinTwo.style.color = "white";
+    }
+  }
 }
 
+//Resets the 2 Minutes Timer
 function resetTimerTwo() {
-  clearInterval(tog);
+  //clearInterval(tog);
+  cSecTwo.style.color = "white";
+  cMinTwo.style.color = "white";
   cSecTwo.innerHTML = "00";
   cMinTwo.innerHTML = "00";
-  startTimerFlag = 1;
+  //startTimerFlag = 1;
   console.log("TimerTwo Reset at " + toMMSS(counterTwo));
   counterTwo = 0;
 }
 
+//Resets BOTH Timers
 function resetTimerSix() {
   clearInterval(tog);
   resetAll();
+  cSecTwo.style.color = "white";
+  cMinTwo.style.color = "white";
   startTimerFlag = 1;
   console.log("Both Timers Reset at " + toMMSS(counterTwo) + " and " + toMMSS(counterSix) + " respectively");
   counterTwo = 0;
   counterSix = 0;
 }
 
+//Starts BOTH Timers
 function startTimerSix() {
   if (startTimerFlag == 1) {
-    tog = setInterval(function() {countup()}, 1000);
+    tog = setInterval(function() {countup()}, 80);
     console.log("Both Timers Started!");
     startTimerFlag = 0;
   }
